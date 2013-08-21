90124aa37422cb1b00805140d2ceec47e4a98f11
rodyhaddad <rody@rodyhaddad.com>
2013-08-20 23:24:59 -0400

    chore(repo): Added package.json, gruntfile and karma.conf

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                autoWatch: false,
+                singleRun: true
+            },
+            autounit: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            }
+        }
+
+    });
+
+    //Load karma plugin
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadTasks('tasks');
+
+    grunt.registerTask('test', ['karma:unit']);
+    grunt.registerTask('autotest', ['karma:autounit']);
+};
