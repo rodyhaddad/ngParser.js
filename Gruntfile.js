2dfe7e4e019efe6dc2be1a9254ba3f1622f8be61
rodyhaddad <rody@rodyhaddad.com>
2014-01-16 13:59:59 -0700

    chore(grunt): add linting and building (dist files)

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
+        destName: "ngParser",
+        LICENSE: grunt.file.read('LICENSE'),
         karma: {
             unit: {
                 configFile: 'karma.conf.js',
@@ -15,6 +16,33 @@ module.exports = function (grunt) {
                 configFile: 'karma.conf.js',
                 autoWatch: true
             }
+        },
+        concat: {
+          options: {
+            separator: '\n\n',
+            banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> \n<%= LICENSE %>\n*/\n\n'
+          },
+          dist: {
+            src: ["src/**.js"],
+            dest: 'dist/<%= destName %>.js'
+          }
+        },
+        uglify: {
+          options: {
+            preserveComments: 'some' //Licences
+          },
+          dist: {
+            files: {
+              'dist/<%= destName %>.min.js': ['dist/<%= destName %>.js']
+            }
+          }
+        },
+        jshint: {
+          files: ['src/**.js'],
+          options: {
+            loopfunc: true,
+            laxbreak: true
+          }
         }
 
     });
@@ -22,9 +50,14 @@ module.exports = function (grunt) {
     //Load karma plugin
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('test', ['karma:unit']);
     grunt.registerTask('autotest', ['karma:autounit']);
 
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['concat', 'uglify']);
+    grunt.registerTask('lint', ['jshint']);
 };
